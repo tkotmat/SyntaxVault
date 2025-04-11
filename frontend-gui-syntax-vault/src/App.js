3d6e53855610598067d0c1be0c77ccab9955e327
Boris <boris.gluchinskiy@gmail.com>
2025-04-11 22:29:00 +0300

    feature/create frontend

---
+++ frontend-gui-syntax-vault/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/App.css';
+import Header from './components/Header'; // Импорт компонента Header
+function App() {
+return (
+<div className="App">
+    <Header /> {/* Использование компонента Header */}
+</div>
+);
+}
+
+export default App;
